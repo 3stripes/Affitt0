80c7631f240a9ab21f92fcc3f7968a0f3e9a0138
Daniel <steven_2_0@hotmail.com>
2016-04-26 12:41:48 +0200

    wasd

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,17 +7,21 @@
                     /* Si Aggiungono delle classi agli identifcatori selezionati*/
                     $("#areaChat").addClass("go");
                     $("#chat").addClass("go");
+                    $("#cerca2").addClass("go");
                     /* Si cancellano le classi agli identifcatori selezionati*/
                     $("#areaChat").removeClass("return");
                     $("#chat").removeClass("return");
+                    $("#cerca2").removeClass("return");
                 }
 
                 if(des==false) {
                     $("#areaChat").addClass("return");
                     $("#chat").addClass("return");
+                    $("#cerca2").addClass("return");
 
                     $("#areaChat").removeClass("go");
                     $("#chat").removeClass("go");
+                    $("#cerca2").removeClass("go");
                 }
 
                 if(des==true){des = false;}
